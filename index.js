f5f606f1b5b4d2c647fb7c1fc393f1fbd801cc8f
Tim Finley <tfinley@hubspot.com>
2015-03-20 12:13:33 -0400

    Render cumulative slow trees as well.
    Eg. sum the selfTime for all nodes with the same name.\
    
    Example output (before fixing pad length):
    
    ```
    Slowest Trees                  | Total
    -------------------------------+----------------
    JadeBenderFilter               | 3007ms
    BenderCompassCompiler          | 2448ms
    DelegateBuildToBrocfilesInReverseDependencyOrder | 1873ms
    CalculateDependenciesFilter    | 1380ms
    
    Slowest Trees (cumulative)     | Total
    -------------------------------+----------------
    BenderCompassCompiler (47)     | 8106ms
    JadeBenderFilter (47)          | 4468ms
    JadeBenderCustomizationsFilter (47) | 2419ms
    TreeMerger (95)                | 1996ms
    DelegateBuildToBrocfilesInReverseDependencyOrder (1) | 1873ms
    CoffeeScriptFilter (47)        | 1849ms
    CalculateDependenciesFilter (1) | 1380ms
    Funnel (423)                   | 1260ms
    ```

--- index.js
+++ index.js
@@ -1,32 +1,74 @@
+function nameFromTreeNode(node) {
+  return node.tree.description || node.tree.constructor.name;
+}
+
 function printSlowTrees(graph, factor) {
-  var sortedTrees = sortResults(graph)
-  var minimumTime = graph.totalTime * (factor || 0.05)
-  var logLines = []
+  try {
+    var allSortResults = sortResults(graph)
+    var flatSortedTrees = allSortResults.flatSortedTrees
+    var groupedSortedTrees = allSortResults.groupedSortedTrees
 
-  for (var i = 0; i < sortedTrees.length; i++) {
-    var node = sortedTrees[i]
-    var name = node.tree.description || node.tree.constructor.name
+    var minimumTime = graph.totalTime * (factor || 0.05)
+    var logLines = [],
+        cumulativeLogLines = [];
 
-    if (node.selfTime > minimumTime) {
-      logLines.push(pad(name, 30) + ' | ' + pad(Math.floor(node.selfTime / 1e6) + 'ms', 15))
+    var MAX_NAME_LENGTH = 30,
+        MAX_VALUE_LENGTH = 15;
+
+    for (var i = 0; i < flatSortedTrees.length; i++) {
+      var node = flatSortedTrees[i]
+      var name = nameFromTreeNode(node)
+
+      if (node.selfTime > minimumTime) {
+        logLines.push(pad(name, MAX_NAME_LENGTH) + ' | ' + pad(Math.floor(node.selfTime / 1e6) + 'ms', MAX_VALUE_LENGTH))
+      }
     }
-  }
 
-  if (logLines.length > 0) {
-    logLines.unshift(pad('', 30, '-') + '-+-' + pad('', 15, '-'))
-    logLines.unshift(pad('Slowest Trees', 30) + ' | ' + pad('Total', 15))
-  }
+    if (logLines.length > 0) {
+      logLines.unshift(pad('', MAX_NAME_LENGTH, '-') + '-+-' + pad('', MAX_VALUE_LENGTH, '-'))
+      logLines.unshift(pad('Slowest Trees', MAX_NAME_LENGTH) + ' | ' + pad('Total', MAX_VALUE_LENGTH))
+    }
 
-  console.log('\n' + logLines.join('\n') + '\n')
+    for (var i = 0; i < groupedSortedTrees.length; i++) {
+      var group = groupedSortedTrees[i]
+
+      if (group.totalSelfTime > minimumTime) {
+        cumulativeLogLines.push(pad(group.name + ' (' + group.nodes.length + ')', MAX_NAME_LENGTH) + ' | ' + pad(Math.floor(group.totalSelfTime / 1e6) + 'ms', MAX_VALUE_LENGTH))
+      }
+    }
+
+    if (cumulativeLogLines.length > 0) {
+      cumulativeLogLines.unshift(pad('', MAX_NAME_LENGTH, '-') + '-+-' + pad('', MAX_VALUE_LENGTH, '-'))
+      cumulativeLogLines.unshift(pad('Slowest Trees (cumulative)', MAX_NAME_LENGTH) + ' | ' + pad('Total', MAX_VALUE_LENGTH))
+      cumulativeLogLines.unshift('\n')
+    }
+
+    console.log('\n' + logLines.join('\n') + cumulativeLogLines.join('\n') + '\n')
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 }
 
 function sortResults(graph) {
   var flattenedTrees = []
+  var treesGroupedByName = Object.create(null)
+  var groupedTrees = [];
 
   function process(node) {
     if (flattenedTrees.indexOf(node) > -1) { return } // for de-duping
 
     flattenedTrees.push(node)
+
+    var name = nameFromTreeNode(node)
+    if (treesGroupedByName[name] == null) {
+      treesGroupedByName[name] = {
+        name: name,
+        nodes: [],
+        totalSelfTime: undefined // to calculate
+      }
+    }
+    treesGroupedByName[name].nodes.push(node)
 
     var length = node.subtrees.length
     for (var i = 0; i < length; i++) {
@@ -34,11 +76,36 @@ function sortResults(graph) {
     }
   }
 
+
   process(graph) // kick off with the top item
 
-  return flattenedTrees.sort(function(a, b) {
+  var flatSortedTrees = flattenedTrees.sort(function(a, b) {
     return b.selfTime - a.selfTime
   })
+
+
+  for (var groupName in treesGroupedByName) {
+    var group = treesGroupedByName[groupName];
+
+    group.totalSelfTime = group.nodes.reduce(function(sum, node) {
+      return sum + node.selfTime
+    }, 0);
+
+    groupedTrees.push(group);
+  }
+
+  var flatSortedTrees = flattenedTrees.sort(function(a, b) {
+    return b.selfTime - a.selfTime
+  })
+
+  var groupedSortedTrees = groupedTrees.sort(function(a, b) {
+    return b.totalSelfTime - a.totalSelfTime
+  })
+
+  return {
+    flatSortedTrees: flattenedTrees,
+    groupedSortedTrees: groupedSortedTrees
+  }
 }
 
 function pad(str, len, char, dir) {
