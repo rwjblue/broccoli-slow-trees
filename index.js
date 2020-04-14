60e2b2f1ca9e9e14f25244c50403c58560cfcff1
Gabriel Csapo <gabecsapo@gmail.com>
2020-04-13 17:17:36 -0700

    expand and condense based on the largest string

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ function ellipsize(string, desiredLength) {
   }
 }
 
+function getLargestString(strings) {
+  return Math.max(...(strings.map(string => string.length)));
+}
+
 module.exports = function printSlowNodes(tree, factor, log) {
   try {
     log = log || console.log;
@@ -17,9 +21,18 @@ module.exports = function printSlowNodes(tree, factor, log) {
     var cumulativeLogLines = [];
 
 
-    var MAX_TABLE_LENGTH = process.stdout.columns || 100;
+    // get the max table length, which is found by looping over all the strings and getting the largest one.
+    // If that is larger than the terminal width we will ellipsize
+    var largestStringLength = getLargestString(summary.groupedNodes.map(function(group) {
+      return group.name
+    })) + 5 // compensate for additional space for group string
+
+    // this is the length of 'Slowest Nodes (totalTime >= 5%)'
+    var MIN_NAME_CELL_LENGTH = 35;
+
+    var MAX_TABLE_LENGTH = process.stdout.columns;
     // the name column is 8/10th the size of the value celll
-    var MAX_NAME_CELL_LENGTH = Math.floor(MAX_TABLE_LENGTH / 2);
+    var MAX_NAME_CELL_LENGTH = largestStringLength < Math.floor(MAX_TABLE_LENGTH / 2) ? Math.max(MIN_NAME_CELL_LENGTH, largestStringLength) : Math.floor(MAX_TABLE_LENGTH / 2);
     // the name column is 2/10th the size of the value celll
     var MAX_VALUE_CELL_LENGTH = Math.floor(MAX_TABLE_LENGTH / 8);
 
--- tests/index.js
+++ tests/index.js
@@ -56,10 +56,62 @@ describe('printSlowNodes', function() {
       return output;
     })).to.eventually.deep.equal([
       '\n' +
-        'Slowest Nodes (totalTime >= 5%)                    | Total (avg) \n' +
-        '---------------------------------------------------+-------------\n' +
-        'merge-trees (2)                                    | 400ms (200 ms)\n' +
-        'babel (1)                                          | 100ms       \n'
+        'Slowest Nodes (totalTime >= 5%)     | Total (avg) \n' +
+        '------------------------------------+-------------\n' +
+        'merge-trees (2)                     | 400ms (200 ms)\n' +
+        'babel (1)                           | 100ms       \n'
+    ]);
+  });
+
+  it('prints slow nodes but only takes up as much space as necessary', function() {
+    stubTime(100);
+    stubProcessColumnSize(70);
+
+    var heimdall = new Heimdall();
+
+    return expect(heimdall.node({ name: 'babel', broccoliNode: true, }, function () {
+      stubTime(200);
+      return heimdall.node({ name: 'merge-trees-larger-name-data', broccoliNode: true }, function () {
+        stubTime(350);
+      });
+    }).then(function () {
+      var output = [];
+
+      printSlowNodes(heimdall, null, function(data) { output.push(data) });
+
+      return output;
+    })).to.eventually.deep.equal([
+      '\n' +
+        'Slowest Nodes (totalTime >= 5%)     | Total (avg)\n' +
+        '------------------------------------+---------\n' +
+        'merge-trees-larger-name-data (1)    | 150ms   \n' +
+        'babel (1)                           | 100ms   \n'
+    ]);
+  });
+
+  it('prints slow nodes should expand based upon process.stdout.column', function() {
+    stubTime(100);
+    stubProcessColumnSize(250);
+
+    var heimdall = new Heimdall();
+
+    return expect(heimdall.node({ name: 'babel', broccoliNode: true, }, function () {
+      stubTime(200);
+      return heimdall.node({ name: 'merge-trees-larger-name-data-with-more-information', broccoliNode: true }, function () {
+        stubTime(350);
+      });
+    }).then(function () {
+      var output = [];
+
+      printSlowNodes(heimdall, null, function(data) { output.push(data) });
+
+      return output;
+    })).to.eventually.deep.equal([
+      '\n' +
+        'Slowest Nodes (totalTime >= 5%)                         | Total (avg)                    \n' +
+        '--------------------------------------------------------+--------------------------------\n' +
+        'merge-trees-larger-name-data-with-more-information (1)  | 150ms                          \n' +
+        'babel (1)                                               | 100ms                          \n'
     ]);
   });
 });
